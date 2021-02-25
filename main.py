45acff43dda9f7d730a75815af2b3792ed575f3a
Hugo Wood <hugo.wood@zenika.com>
2021-02-25 21:34:30 +0100

    more accurate weight?

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ import json
 import os
 import sys
 
+from math import gcd
+from collections import defaultdict
+
+
+
 data_directory = "./data_sets_json/"
 data_set_files = os.listdir(data_directory)
 
 
 def main():
     for i, data_set_file in enumerate(data_set_files):
+        # if i != 1:
+        #     continue
         filename = os.path.join(data_directory, data_set_file)
         with open(filename) as current_file:
             data = json.load(current_file)
@@ -54,19 +61,54 @@ def compose_streets_by_intersection(streets):
 
 def handle_dataset(data):
     streets = data["streets"]
+    streets_by_name = {
+        street['name']: street
+        for street in data['streets']
+    }
     streets_by_intersection = compose_streets_by_intersection(streets)
-    car_weights = compose_car_weights(data)
-    return [(intersection, [(street, car_weights[street]) for street in streets if street in car_weights] or [(streets[0], 1)]) for intersection, streets in streets_by_intersection.items()]
 
-def compose_car_weights(data):
+    car_journeys = {
+        i:
+            sum(
+                streets_by_name[street]['length']
+                for street in car['path']
+            )
+        for i, car in enumerate(data["cars"])
+    }
+    car_weights = compose_car_weights(data, streets_by_name, car_journeys)
+    # print(car_weights)
+    return [
+        (
+            intersection,
+            [
+                (street_name, min(data["duration"], round(car_weights[intersection][street_name] / min(car_weights[intersection].values()))))
+                for street_name in streets if street_name in car_weights[intersection]
+            ] or [(streets[0], 1)]
+        ) for intersection, streets in streets_by_intersection.items()
+    ]
+
+def compose_car_weights(data, streets_by_name, car_journeys):
     cars = data["cars"]
-    result = {}
-    for car in cars:
-        for street in car["path"]:
-            if street not in result:
-                result[street] = 0
-            result[street] += 1
+    result = defaultdict(lambda: defaultdict(int))
+    out_of_time_cars = compose_out_of_time_cars(data, streets_by_name)
+    for i, car in enumerate(cars):
+        if i in out_of_time_cars:
+            continue
+        for street_name in car["path"]:
+            journey_length = car_journeys[i]
+            weight = journey_length / data["duration"]
+            result[streets_by_name[street_name]["end"]][street_name] += weight
     return result
+
+def compose_out_of_time_cars(data, streets_by_name):
+    for i, car in enumerate(data['cars']):
+        s = sum(
+            streets_by_name[street]['length']
+            for street in car['path'])
+        # print(f"car {i}: {s}")
+        if s > data['duration']:
+            yield i
+
 
 ################################################################
 main()
